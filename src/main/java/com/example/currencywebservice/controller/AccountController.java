5b1462c72e51b10ec928c336c9911addda283b3c
MHR1985 <cph-mr221@cphbusiness.dk>
2020-10-02 21:30:01 +0200

    added two methods

--- src/main/java/com/example/currencywebservice/controller/AccountController.java
+++ src/main/java/com/example/currencywebservice/controller/AccountController.java
@@ -17,21 +17,36 @@ public class AccountController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Account> findAccount(@PathVariable Long id) throws ResourceNotFoundException {
-        Account account =  accountRepository.findByAccountNumber(id);
+        Account account = accountRepository.findByAccountNumber(id);
         return new ResponseEntity<>(account, HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Account> createAccount(@RequestBody Account account){
+    public ResponseEntity<Account> createAccount(@RequestBody Account account) {
         accountRepository.save(account);
         return new ResponseEntity<>(account, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Account> deleteAccount(@PathVariable Long id){
+    public ResponseEntity<Account> deleteAccount(@PathVariable Long id) {
         Account account = accountRepository.findByAccountNumber(id);
         accountRepository.delete(account);
         return new ResponseEntity<>(account, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Account> updateAccount(@PathVariable Long id, @RequestBody Account account) {
+        Account oldAccount = accountRepository.findByAccountNumber(id);
+        oldAccount.amount = account.amount;
+        oldAccount.currency = account.currency;
+        accountRepository.save(oldAccount);
+        return new ResponseEntity<>(oldAccount, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Account>> getAccounts() {
+        List<Account> accounts = accountRepository.getAllAccounts();
+        return new ResponseEntity<>(accounts, HttpStatus.OK);
     }
 
 }
